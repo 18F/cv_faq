2e5a5088cd97862742304fc9da04853c8a48e2b9
David Corwin <david.corwin@gsa.gov>
2020-03-17 15:34:49 -0700

    generate netlify cms config from collections

---
+++ _plugins/jekyll_config_netlify_cms.rb
@@ -0,0 +1,40 @@
+require 'yaml'
+
+NETLIFY_CONFIG_DEST_DIR = File.join('_site', 'admin')
+NETLIFY_CONFIG_SOURCE_DIR = 'admin'
+
+Jekyll::Hooks.register :site, :post_write do |site|
+  collections = site.collections['content'].docs
+    .map { |doc| doc.relative_path.split('/')[0..-2].join('/') }
+    .uniq
+    .map do |path|
+      {
+        'label' => path.split('/')[1..].join(' | ').gsub(/-/, ' '),
+        'name' => path.split('/')[1..].join('--'),
+        'folder' => path,
+        'create' => true,
+        'slug' => '{{slug}}',
+        'fields' => [
+          {'label' => 'Layout', 'name' => 'layout', 'widget' => 'hidden', 'default' => 'post'},
+          {'label' => 'Title', 'name' => 'title', 'widget' => 'string'},
+          {'label' => 'Tag', 'name' => 'tag', 'widget' => 'string'},
+          {
+            'label' => 'Publish Date',
+            'name' => 'date',
+            'widget' => 'datetime',
+            'format' => 'MMMM D, YYYY',
+            'dateFormat' => 'MMMM D, YYYY',
+            'timeFormat' => false
+          },
+          {'label' => 'Excerpt', 'name' => 'excerpt', 'widget' => 'string'},
+          {'label' => 'Body', 'name' => 'body', 'widget' => 'markdown'}
+        ]
+      }
+    end
+  
+  
+  config = {}
+  File.open(File.join(NETLIFY_CONFIG_SOURCE_DIR, 'config.yml')) { |file| config = YAML.load(file) }
+  config['collections'] = config['collections'] + collections
+  File.open("#{NETLIFY_CONFIG_DEST_DIR}/config.yml", 'w') { |file| file.write(config.to_yaml) }
+end
