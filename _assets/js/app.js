fea4d54e3a7d19bd65ee4a253868763f580a107d
Andrew Dunkman <andrew.dunkman@gsa.gov>
2020-03-18 11:42:45 -0400

    Remove javascript.
    This was throwing an error as implemented.

--- _assets/js/app.js
+++ _assets/js/app.js
@@ -1,46 +1,4 @@
 //= require uswds/dist/js/uswds.min
-//= require ./simple-jekyll-search
-
-(function () {
-  // const form = document.getElementById('search_form');
-  if (form) {
-    const urlParams = new URLSearchParams(window.location.search);
-    const autocomplete = document.getElementById('autocomplete-container');
-    const fullPageResults = document.getElementById('search-results');
-    const searchBox = document.getElementById('search-box');
-
-    const sjs = SimpleJekyllSearch({
-      searchInput: searchBox,
-      resultsContainer: fullPageResults || autocomplete,
-      json: form.action + '.json',
-      noResultsText: 'No results found.',
-      searchResultTemplate: '<li class="padding-1 font-sans-md"><a href="{url}" title="{desc}">{title}</a></li>',
-      templateMiddleware: function (prop, value, template) {
-        if (prop === 'url') return value;
-
-        const inputValue = searchBox.value;
-        const regex = new RegExp(inputValue, 'i');
-
-        const output = value.replace(regex, function (v) {
-          return '<b>' + v + '</b>'
-        });
-        return output
-      },
-      limit: 10,
-      fuzzy: true
-    });
-
-    const query = urlParams.get('query');
-
-    if (query) {
-      window.addEventListener('load', function () {
-        const searchInput = document.querySelector('#search-box');
-        searchInput.setAttribute("value", query);
-        sjs.search(query);
-      });
-    }
-  }
-})();
 
 const EXPANDED = "aria-expanded";
 
