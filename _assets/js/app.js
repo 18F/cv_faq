9af893803c803cf1de272a8fcbb349cb1a22e91f
Andrew Dunkman <andrew.dunkman@gsa.gov>
2020-03-18 21:16:36 -0400

    Add accessible-typeahead, use for typeahead.

--- _assets/js/app.js
+++ _assets/js/app.js
@@ -3,55 +3,6 @@
 
 
 //////////////////////////////////
-// Type Ahead Input
-(function () {
-  window.addEventListener('DOMContentLoaded', () => {
-    const renderResults = (results, elementId = 'autocomplete-results') => {
-      const element = document.getElementById(elementId);
-
-      if (!results.length) {
-        element.innerHTML = renderResults_noResults();
-      }
-      else {
-        element.innerHTML = `
-          <ol>
-            ${results.map(renderResults_result).join('')}
-          </ol>
-        `;
-      }
-    };
-
-    const renderResults_noResults = () => {
-      return '';
-    };
-
-    const renderResults_result = (result) => {
-      return `
-        <li>
-          <a href="${result.url}">
-            <p class="title">${highlight(result.title)}</p>
-          </a>
-        </li>
-      `;
-    };
-
-    const highlight = (text) => {
-      return text.replace(/\uE000/g, '<strong>').replace(/\uE001/g, '</strong>');
-    };
-
-    const typeAheadInput = document.getElementById('search-box');
-    const typeAheadSearch = query => window.TypeAheadSearch(query)
-      .then(renderResults);
-    const typeAheadDebounce = window.Debounce(typeAheadSearch);
-
-    if (typeAheadInput) {
-      typeAheadInput.addEventListener('input', e => typeAheadDebounce(e.target.value));
-    }
-  });
-})();
-
-
-//////////////////////////////////
 // Accordions
 const EXPANDED = "aria-expanded";
 
