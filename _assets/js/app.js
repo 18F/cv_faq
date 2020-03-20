604b6cc14a4165afb45178a2873cfc1c441c5359
Joe Krzystan <joseph.krzystan@gsa.gov>
2020-03-19 21:11:53 -0400

    Show more questions on button click

--- _assets/js/app.js
+++ _assets/js/app.js
@@ -18,3 +18,26 @@
     (target || accordionButtons[0]).setAttribute(EXPANDED, true);
   }
 })();
+
+//////////////////////////////////
+// Load more questions
+var wrapper = document.querySelector('#load-questions-wrapper')
+var button = document.querySelector('[load-questions]');
+var content = document.querySelector('#' + button.getAttribute('aria-controls'));
+
+if (content) {
+  content.classList.add('display-none');
+  wrapper.classList.remove('display-none');
+
+
+  document.addEventListener('click', function (event) {
+
+    var content = document.querySelector('#' + event.target.getAttribute('aria-controls'));
+
+      event.target.setAttribute('aria-expanded', true);
+      content.classList.remove('display-none');
+      wrapper.classList.add('display-none');
+
+  });
+}
+
