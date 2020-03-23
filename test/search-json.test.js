0366469009eddd19af22b453c702932f60686c63
Andrew Dunkman <andrew.dunkman@gsa.gov>
2020-03-23 10:57:08 -0400

    Assert case sensitivity of paths.

--- test/search-json.test.js
+++ test/search-json.test.js
@@ -1,9 +1,8 @@
-const { promisify } = require('util');
-const fs = require('fs');
-const access = promisify(fs.access);
+const path = require('path');
+const { trueCasePath } = require('true-case-path');
 
 const convertRelativeUrlToPath = (relativeUrl) => {
-  const prefix = './_site';
+  const prefix = '_site';
   const suffix = isFolder(relativeUrl) ? 'index.html' : '';
 
   return `${prefix}${relativeUrl}${suffix}`;
@@ -14,6 +13,11 @@ const isFolder = (path) => path[path.length - 1] === '/';
 test('search.json contains valid URLs', async () => {
   const search = require('../_site/search.json');
   const paths = search.map(item => convertRelativeUrlToPath(item.url));
+  const cwd = `${process.cwd()}${path.sep}`;
 
-  await Promise.all(paths.map(p => access(p)));
+  return await Promise.all(paths.map(async (requestedPath) => {
+    const pathOnFilesystem = await trueCasePath(requestedPath);
+    const relativePathOnFilesystem = pathOnFilesystem.replace(cwd, '');
+    expect(requestedPath).toBe(relativePathOnFilesystem);
+  }));
 });
