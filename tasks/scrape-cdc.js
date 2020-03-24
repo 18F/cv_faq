84752653764ce3e98a821a0d3cec6ae47b4dcfb9
Andrew Dunkman <andrew.dunkman@gsa.gov>
2020-03-24 13:55:26 -0400

    Add data scraping task.

---
+++ tasks/scrape-cdc.js
@@ -0,0 +1,36 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+
+  try {
+    const page = await browser.newPage();
+    const content = await scrapeFaq(page);
+
+    console.log(JSON.stringify(content, null, 2));
+  }
+  finally {
+    await browser.close();
+  }
+})();
+
+const scrapeFaq = async (page) => {
+  await page.goto('https://www.cdc.gov/coronavirus/2019-ncov/faq.html');
+
+  const data = await page.$$eval('.col.content h2', headings => headings.map(heading => {
+    const accordion = heading.nextElementSibling;
+    const items = Array.from(accordion.querySelectorAll('.card-header'));
+
+    return {
+      title: heading.innerText,
+      items: items.map(question => {
+        return {
+          question: question.innerText,
+          answer: document.querySelector(`${question.dataset.target} .card-body`).innerHTML.trim()
+        };
+      })
+    };
+  }));
+
+  return data;
+};
