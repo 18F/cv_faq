6bf2463ea625c2b8e289e8177667c610826bf5fe
Daniel Naab <dnaab@flexion.us>
2020-06-10 10:34:12 -0500

    On preset-env config, set useBuiltIns: usage, and shore up Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,33 @@
-const TerserPlugin = require('terser-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-
-module.exports = {
+const config = {
   entry: [
     'whatwg-fetch',
-    'core-js/stable',
-    'regenerator-runtime/runtime',
-
     './scripts/app.js'
   ],
   output: {
     filename: 'bundle.js',
     libraryTarget: 'umd'
   },
-  devtool: 'source-map',
-  plugins: [new TerserPlugin()],
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: /@babel(?:\/|\\{1,2})runtime|core-js/,
+        exclude: [
+          // With useBuiltIns: 'usage', exclude polyfills
+          /\bcore-js\b/,
+          /\bwebpack\/buildin\b/,
+
+          // Accessible-autocomplete's bundle is already transpiled
+          /\baccessible-autocomplete\b/
+        ],
         use: {
           loader: 'babel-loader',
           options: {
             presets: [
               ['@babel/preset-env', {
                 corejs: 3,
-                useBuiltIns: 'entry',
-                targets: '>0.2%, not dead'
+                useBuiltIns: 'usage'
               }]
             ]
           }
@@ -36,3 +36,35 @@ module.exports = {
     ]
   }
 };
+
+module.exports = (env, argv) => {
+  if (argv.mode === 'development') {
+    config.devtool = 'source-map';
+  }
+  else if (argv.mode === 'production') {
+    config.optimization = {
+      minimize: true,
+      minimizer: [new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true,
+        uglifyOptions: {
+          compress: {
+            negate_iife: false,
+            properties: false,
+            ie8: true
+          },
+          mangle: {
+            ie8: true
+          },
+          output: {
+            comments: false,
+            ie8: true
+          }
+        }
+      })]
+    };
+  }
+
+  return config;
+}
