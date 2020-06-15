a25406ae9bf7a7d41c979bc6704daf877a41afd5
Daniel Naab <dnaab@flexion.us>
2020-06-15 15:50:43 -0500

    Add pagination support to search results page.

--- scripts/components/search-results.js
+++ scripts/components/search-results.js
@@ -12,36 +12,89 @@ export const initSearchResults = () => {
   const query = searchParams.get('query');
   const routedFrom = searchParams.get('source');
 
-  const highlightSearchTerms = !routedFrom;
-  SearchService(query, highlightSearchTerms).then((response) => {
+  let nextOffset = null;
+
+  searchResultsContainer.classList.toggle('is-loading', true);
+  SearchService({
+    query,
+    highlightSearchTerms: !routedFrom,
+    searchTimeoutSeconds: 3
+  }).then((response) => {
+    nextOffset = response.nextOffset;
     if (response.routeTo) {
       window.location.replace(response.routeTo);
     }
     else {
-      renderResults(searchResultsContainer, query, response.results, routedFrom);
+      renderResults({
+        searchResultsContainer,
+        results: response.results,
+        resultsCount: response.resultsCount,
+        nextOffset,
+        query,
+        routedFrom
+      });
     }
+  });
+
+  searchResultsContainer.querySelector('.more-results-button').addEventListener('click', () => {
+    if (!nextOffset) {
+      return;
+    }
+    SearchService({
+      query,
+      offset: nextOffset,
+      highlightSearchTerms: !routedFrom,
+      searchTimeoutSeconds: null
+    }).then((response) => {
+      nextOffset = response.nextOffset;
+      renderResults({
+        searchResultsContainer,
+        results: response.results,
+        resultsCount: response.resultsCount,
+        nextOffset,
+        query,
+        routedFrom
+      });
+    });
   });
 };
 
-const renderResults = (searchResultsContainer, query, results, routedFrom) => {
-  searchResultsContainer.innerHTML = '';
+const renderResults = ({
+  searchResultsContainer,
+  results,
+  resultsCount,
+  nextOffset,
+  query,
+  routedFrom
+}) => {
+  searchResultsContainer.classList.toggle('is-routed-query', !!routedFrom);
+  searchResultsContainer.classList.toggle('has-more-results', !!nextOffset);
+  searchResultsContainer.classList.toggle('is-loading', false);
 
-  if (routedFrom) {
-    searchResultsContainer.innerHTML += `
-      <h2 class="title">We’re sorry! We found 0 results for “${routedFrom}.”</h2>
-      <h2 class="title">However, we found results for the related term “${query}.”</h2>
+  if (resultsCount) {
+    const resultsHeader = searchResultsContainer.querySelector('.results-header');
+    resultsHeader.innerHTML = `
+      <div class="grid-col-fill">
+        <h1>Search Results <span class="num-results">(${resultsCount})</span></h1>
+      </div>
     `;
+    if (nextOffset) {
+      resultsHeader.innerHTML += `
+        <div class="results-summary grid-col-auto">Displaying 1-${nextOffset - 1} of ${resultsCount}</div>
+      `;
+    }
   }
+
+  searchResultsContainer.querySelector('.routed-from').innerHTML = routedFrom;
+  searchResultsContainer.querySelector('.query').innerHTML = query;
 
   if (!results.length) {
-    searchResultsContainer.innerHTML += `<h2 class="title">No results found</h2>`;
+    searchResultsContainer.innerHTML += `<div>No results found</div>`;
   }
   else {
-    searchResultsContainer.innerHTML += `
-      <ol>
-        ${results.map(renderResults_result(routedFrom)).join('')}
-      </ol>
-    `;
+    searchResultsContainer.querySelector('ol.results-list').innerHTML += results.map(
+      renderResults_result()
+     ).join('');
   }
 };
 
@@ -52,21 +105,17 @@ const continueReading = (result) => {
   return '';
 }
 
-const renderResults_result = (routedFrom) => (result) => {
+const renderResults_result = () => (result) => {
   return `
     <li>
       <a href="${result.url}">
-        <h2 class="title">${highlight(result.title, routedFrom)}</h2>
-        <p>${highlight(result.description, routedFrom)}${continueReading(result)}</p>
+        <h2 class="title">${highlight(result.title)}</h2>
+        <p>${highlight(result.description)}${continueReading(result)}</p>
       </a>
     </li>
   `;
 };
 
-const highlight = (text, routedFrom) => {
-  // Don't highlight text on routed search URLs
-  if (routedFrom) {
-    return text;
-  }
+const highlight = (text) => {
   return text.replace(/\uE000/g, '<span class="bg-yellow">').replace(/\uE001/g, '</span>');
 };
