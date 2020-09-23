ce2c018584b4a94bd61615038fbe7e91f8be5631
Daniel Naab <dnaab@flexion.us>
2020-09-23 13:34:46 -0500

    Added category groupings to home page.

--- test/index.html.test.js
+++ test/index.html.test.js
@@ -46,41 +46,24 @@ test('both question boxes have three content links and a view all link', async (
   });
 });
 
-test('the six displayed categories have three content links and a view all link', async () => {
-  const categories = await page.$$eval('.top-categories .top-category', elements => {
+test('the displayed categories have content links and a description', async () => {
+  const category_groupings = await page.$$eval('.top-categories .top-category', elements => {
     return elements.map(b => {
       const getText = (el) => el ? el.innerText.trim().replace(/\n/g, '') : null;
 
       return {
         title: getText(b.querySelector('h3')),
-        questions: [...b.querySelectorAll('li')].map(getText),
-        viewAllText: getText(b.querySelector('.view-all'))
+        description: getText(b.querySelector('p')),
+        categories: [...b.querySelectorAll('li')].map(getText)
       };
     });
   });
 
-  expect(categories).toHaveLength(6);
+  expect(category_groupings).toHaveLength(6);
 
-  categories.forEach(({title, questions, viewAllText}) => {
+  category_groupings.forEach(({title, description, categories}) => {
     expect(title).not.toEqual('');
-    expect(questions).toHaveLength(3);
-
-    // Assert that the view all text (including the screen reader text) is present.
-    expect(viewAllText).toContain(`View all questions about ${title}`);
+    expect(description).not.toEqual('');
+    expect(categories.length).toBeGreaterThan(1);
   });
-});
-
-test('the load more button displays additional categories', async () => {
-  const getVisibleCategories = () => page.$$eval('.top-category', elements => {
-    return elements.map(category => { return {
-      title: category.querySelector('h3').innerText,
-      visible: category.offsetHeight !== 0
-    }}).filter(c => c.visible);
-  });
-
-  const before = await getVisibleCategories();
-  await page.click('#more-questions-button');
-  const after = await getVisibleCategories();
-
-  expect(before.length).toBeLessThan(after.length);
 });
