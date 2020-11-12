e42d16ad755fc629d209212d4d05dd674e9d6228
Taylor Zajicek <tzajicek@flexion.us>
2020-11-12 13:44:42 -0600

    Updated search.gov docs

--- scripts/services/search/click-tracking.js
+++ scripts/services/search/click-tracking.js
@@ -3,26 +3,38 @@
  * https://docs.google.com/document/d/1ZrRczcb-tIiklxd92vJpGiZKeRCe1ll71_zXhlp8Dj0/
  */
 
-import { SEARCHGOV_ACCESS_KEY, SEARCHGOV_AFFILIATE } from './constants';
+import { SEARCHGOV_ACCESS_KEY, SEARCHGOV_AFFILIATE, SEARCHGOV_MODULE } from './constants';
 
 const CLICK_TRACK_URL = 'https://api.gsa.gov/technology/searchgov/v2/clicks/';
 
 export const trackClick = ({url, query, position}) => {
   const clickEndpoint = new URL(CLICK_TRACK_URL);
 
-  // Construct a query string for this request
-  Object.entries({
+  const formData = {
     url,
     query,
     affiliate: SEARCHGOV_AFFILIATE,
     position,
-    module_code: 'i14y',
+    module_code: SEARCHGOV_MODULE,
     access_key: SEARCHGOV_ACCESS_KEY,
-    client_ip: '0.0.0.0',
-    user_agent: navigator.userAgent,
     referrer: window.location.href,
-  }).forEach(([key, value]) => clickEndpoint.searchParams.append(key, value));
+  };
 
-  // Do pingback - no need to wait for response
-  fetch(clickEndpoint);
+  // Construct a query string for this request
+  Object.entries(formData).forEach(([key, value]) => clickEndpoint.searchParams.append(key, value));
+
+  const goToResult = () => window.location.href = url;
+  const timeout = new Promise((resolve, reject) => setTimeout(resolve, 500));
+
+  // Do pingback
+  Promise.race(
+    fetch(clickEndpoint.toString(), {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: '',
+    }),
+    timeout
+  ).then(goToResult, goToResult);
 };
